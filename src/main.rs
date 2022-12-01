d8b89d9ed5bdd546afb39b7bfe84d5ad02791658
Nam Nguyen <nam.nguyen@indebted.co>
2022-12-01 17:26:14 +1000

    day1 part2

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::io::{self, BufRead, BufReader};
 fn main() -> io::Result<()> {
     let reader = BufReader::new(File::open("inputs/day1.txt").expect("Cannot open file"));
 
-    let mut max = 0;
+    let mut top_three = vec![0, 0, 0];
     let mut total = 0;
     for line in reader.lines() {
         let s: String = line.unwrap();
@@ -14,10 +14,22 @@ fn main() -> io::Result<()> {
             let cal: i32 = s.parse().unwrap();
             total += cal;
         }
-        if total > max {
-            max = total;
+        top_three = replace_min(top_three, total);
+    }
+    println!("Top 3: {:?}", top_three);
+    let sum: i32 = top_three.iter().sum();
+    println!("Sum: {}", sum);
+    Ok(())
+}
+
+fn replace_min(mut v: Vec<i32>, n: i32) -> Vec<i32> {
+    let min = v.iter().min().unwrap();
+    if *min < n {
+        let index = v.iter().position(|x| x == min);
+        match index {
+            Some(i) => v[i] = n,
+            None => (),
         }
     }
-    println!("{}", max);
-    Ok(())
+    v
 }
