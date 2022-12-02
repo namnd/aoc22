fc4754c8da983368ed2acc245a81ec4b6a651753
Nam Nguyen <nam.nguyen@indebted.co>
2022-12-02 15:53:36 +1000

    day2 part2

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,19 @@ fn main() -> io::Result<()> {
     let reader = BufReader::new(File::open("inputs/day2.txt").expect("Cannot open file"));
 
     let mut score_map = HashMap::new();
-    score_map.insert("X", 1);
-    score_map.insert("Y", 2);
-    score_map.insert("Z", 3);
+    score_map.insert("X", 0);
+    score_map.insert("Y", 3);
+    score_map.insert("Z", 6);
     let mut outcome_map = HashMap::new();
     outcome_map.insert("A X", 3);
-    outcome_map.insert("A Y", 6);
-    outcome_map.insert("A Z", 0);
-    outcome_map.insert("B X", 0);
-    outcome_map.insert("B Y", 3);
-    outcome_map.insert("B Z", 6);
-    outcome_map.insert("C X", 6);
-    outcome_map.insert("C Y", 0);
-    outcome_map.insert("C Z", 3);
+    outcome_map.insert("A Y", 1);
+    outcome_map.insert("A Z", 2);
+    outcome_map.insert("B X", 1);
+    outcome_map.insert("B Y", 2);
+    outcome_map.insert("B Z", 3);
+    outcome_map.insert("C X", 2);
+    outcome_map.insert("C Y", 3);
+    outcome_map.insert("C Z", 1);
 
     let mut total_scores = 0;
     for line in reader.lines() {
