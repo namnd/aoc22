9a5ac8b81bee500db435a61f376f49ade7fbaf02
Nam Nguyen <nam.nguyen@indebted.co>
2022-12-02 15:45:34 +1000

    day2 part1

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,37 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
 fn main() -> io::Result<()> {
-    let reader = BufReader::new(File::open("inputs/day1.txt").expect("Cannot open file"));
+    let reader = BufReader::new(File::open("inputs/day2.txt").expect("Cannot open file"));
 
-    let mut top_three = vec![0, 0, 0];
-    let mut total = 0;
+    let mut score_map = HashMap::new();
+    score_map.insert("X", 1);
+    score_map.insert("Y", 2);
+    score_map.insert("Z", 3);
+    let mut outcome_map = HashMap::new();
+    outcome_map.insert("A X", 3);
+    outcome_map.insert("A Y", 6);
+    outcome_map.insert("A Z", 0);
+    outcome_map.insert("B X", 0);
+    outcome_map.insert("B Y", 3);
+    outcome_map.insert("B Z", 6);
+    outcome_map.insert("C X", 6);
+    outcome_map.insert("C Y", 0);
+    outcome_map.insert("C Z", 3);
+
+    let mut total_scores = 0;
     for line in reader.lines() {
         let s: String = line.unwrap();
-        if s == "" {
-            total = 0;
-        } else {
-            let cal: i32 = s.parse().unwrap();
-            total += cal;
-        }
-        top_three = replace_min(top_three, total);
-    }
-    println!("Top 3: {:?}", top_three);
-    let sum: i32 = top_three.iter().sum();
-    println!("Sum: {}", sum);
-    Ok(())
-}
+        let play = s.split(" ");
 
-fn replace_min(mut v: Vec<i32>, n: i32) -> Vec<i32> {
-    let min = v.iter().min().unwrap();
-    if *min < n {
-        let index = v.iter().position(|x| x == min);
-        match index {
-            Some(i) => v[i] = n,
-            None => (),
+        let our = play.last();
+        if let Some(score) = our {
+            total_scores += score_map[score];
         }
+        total_scores += outcome_map[s.as_str()];
     }
-    v
+    println!("{}", total_scores);
+
+    Ok(())
 }
