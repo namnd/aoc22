34a8a08c30d1bab4320bf444190f6060a5713863
Nam Nguyen <nam.nguyen@indebted.co>
2022-12-06 21:19:04 +1000

    day6 part2

--- src/day6.rs
+++ src/day6.rs
@@ -2,17 +2,30 @@ pub fn part1() {
     let input = aoc22::read_lines_from_file("day6", false).unwrap();
 
     let bytes = input.get(0).expect("Invalid input").as_bytes();
-    if bytes.len() >= 4 {
-        for i in 0..bytes.len() - 4 {
-            let x = &bytes[i..(i + 4)];
-            if is_marker(x) {
-                println!("{:?}", i + 4);
+    const MSG_SIZE: usize = 14;
+    if bytes.len() >= MSG_SIZE {
+        for i in 0..bytes.len() - MSG_SIZE {
+            let x = &bytes[i..(i + MSG_SIZE)];
+            if is_marker_recursive(x) {
+                println!("{:?}", i + MSG_SIZE);
                 break;
             }
         }
     }
 }
 
-fn is_marker(s: &[u8]) -> bool {
-    s[0] != s[1] && s[0] != s[2] && s[0] != s[3] && s[1] != s[2] && s[1] != s[3] && s[2] != s[3]
+fn is_marker_recursive(s: &[u8]) -> bool {
+    let (first, rest) = s.split_first().unwrap();
+    if rest.len() == 0 {
+        return true;
+    }
+    if !is_marker_recursive(rest) {
+        return false;
+    }
+    for e in rest {
+        if first == e {
+            return false;
+        }
+    }
+    true
 }
